6f20f96aa146484f30236f6b3fc566705dd85ab3
alva <alva-seal@users.noreply.github.com>
2024-08-31 12:10:04 +0200

    corrected import

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 # An object of Flask class is our WSGI application.
 from flask import Flask
-import tolino-sync
+from tolino-sync import main
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -26,4 +26,4 @@ if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(host="0.0.0.0", port=1337)
+    app.run(debug=True; host="0.0.0.0", port=1337)
